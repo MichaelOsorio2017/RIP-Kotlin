ae3b55e8634011334abaaf41b4e6367b5d83c44b
Michael Osorio <ms.osorio@uniandes.edu.co>
2019-07-12 00:23:00 -0500

    Class Emulator helper donegit add .git add .

--- src/main/kotlin/helper/EmulatorHelper.kt
+++ src/main/kotlin/helper/EmulatorHelper.kt
@@ -1,13 +1,15 @@
 package helper.EmulatorHelper
-import com.intellij.diff.comparison.expandWhitespaces
 import com.intellij.formatting.blocks.split
 import main.kotlin.RipException
 import helper.ExternalProcess2.executeProcess2
 import helper.Helper
 import java.io.BufferedReader
 import java.io.File
+import java.io.InputStream
 import java.io.InputStreamReader
 import java.lang.Exception
+import kotlin.concurrent.thread
+import kotlin.random.Random
 
 //get the try codes and tries to do the action and throws the exception if something goes wrong
 inline fun simpleTryCatch(action: () -> Unit){
@@ -192,10 +194,9 @@ fun isEventIdle(){
 
     while(!termino){
         pss = pBB.start()
-        reader = BufferedReader(InputStreamReader(pss.inputStream))
-        //por cada linea en el reader las concatena con resp
-        reader.forEachLine { resp += it }
-
+        BufferedReader(InputStreamReader(pss.inputStream)).use {
+            it.forEachLine { resp += it }
+        }
         pss.waitFor()
 
         if(resp.contains("IDLE")){
@@ -283,6 +284,17 @@ fun scroll(coordX1: String = "168", coordY1: String = "680",coordX2: String = "2
 fun goBack(){
     val commands = listOf("adb", "shell", "input", "keyevent", "4")
     executeProcess2(commands, "GO BACK", null, null)
+}
+
+/**
+ * Simulates the effect of touching home soft button, return home. 3 is
+ * KEYCODE_HOME
+ * @throws RipException
+ * @throws IOException
+ */
+fun goHome(){
+    val commands = listOf("adb", "shell", "input", "keyevent", "3")
+    executeProcess2(commands, "GO HOME", null, null)
 }
 
 /**
@@ -444,7 +456,7 @@ fun showNumKeyboard(){
  *            is the string that the users wants to write
  */
     //TODO guardar los valores ingresados en cada input field para luego usarlos en el replay
-fun enterInput(input: String){
+fun enterInput(input: String = "Hola"){
     val newString = input.replace(" ", "%s")
     val commands = listOf("adb", "shell", "input", "text", newString)
     executeProcess2(commands,"INPUT TEXT", null, null )
@@ -557,5 +569,491 @@ fun showBatteryLevel(): Int {
             println("ERROR PARSING BATTERY LEVEL: $level")
             101
         }
+    }
+}
+
+/**
+ * Shows the battery temperature
+ *
+ * @return temperature of the battery in ÂªC
+ * @throws Exception
+ *             is there i no device or emulator
+ */
+
+fun showBatteryTemperature(): Double{
+    val commands = listOf("adb", "shell", "dumpsys", "battery", "|", "grep", "temperature")
+    val answer = executeProcess2(commands, "SHOW BATTERY TEMPERATURE", null, null)
+    val ans = with(answer.first()){
+        val temp = split(":").first().replace("\\s+", "")
+        temp.toDouble()/10
+    }
+    println("TEM: $ans")
+    return ans
+}
+
+/**
+ * Indicates if wifi is enabled in device or emulator
+ *
+ * @return true if wifi is enable, false if is disabled
+ * @throws Exception
+ *             if something goes wrong in executeProcess
+ */
+fun isWifiEnabled(): Boolean{
+    val commands = listOf("adb", "shell", "dumpsys", "wifi", "|", "grep", "'Wi-Fi is'")
+    val answer = executeProcess2(commands, "CHEKING IF WIFI IS ENABLED", null, null)
+    val ans = with(answer.first().split("\n")){
+       first().startsWith("Wi-Fi is enabled")
+    }
+    println("WIFI: $ans")
+    return ans
+}
+
+/**
+ * Indicates is airplane mode is on
+ *
+ * @return true if airplane mode is on, false is airplane mode is off
+ * @throws Exception
+ *             if there is no device or emulator
+ */
+fun isAirplaneModeOn(): Boolean{
+    val commands = listOf("adb", "shell", "dumpsys", "wifi", "|", "grep", "'mAirplaneModeOn'")
+    val answer = executeProcess2(commands, "CHEKING IF AIRPLANE MODE IS ON", null, null)
+    val ans = with(answer.first()){
+        split(" ").first().contains("true")
+    }
+    println("AIR: $ans")
+    return ans
+}
+/**
+ * Verify if the android version of device or emulator is correct, kitkat and
+ * superiors.
+ *
+ * @return true if version is kitkat or superior, false in the other case
+ * @throws Exception
+ *             if version is inferior to kitkat
+ */
+fun verifyAndroidVersion(): Boolean{
+    val commands = listOf("adb", "shell", "getprop", "ro.build.version.release")
+    val answer = executeProcess2(commands, "CHEKING ANDROID VERSION", null, null)
+    val ans = with(answer.first()){
+        if(startsWith("1")||startsWith("2") || startsWith("3") || startsWith("4.1") || startsWith("4.2") || startsWith("4.3")){
+            throw Exception("This program does not support the Android version + $this")
+        }else{
+            true
+        }
+    }
+    println("Android version supported: $ans")
+    return ans
+}
+/**
+ * Gets android version
+ *
+ * @return Android version (Ex: 8.0.1)
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *
+ *
+ */
+fun getAndroidVersion(): String{
+    val commands = listOf("adb", "shell", "getprop", "ro.build.version.release")
+    val answer = executeProcess2(commands, "CHECKING ANDROID VERSION", null, null)
+    return answer.first().replace("\\s+", "")
+}
+/**
+ * Generates a random quantity of graphic methods waiting a certain time
+ *
+ * @param qtyEvents
+ *            is the quantity of methods to generate
+ * @param time
+ *            in milliseconds between methods
+ * @throws Exception
+ *             if any of the methods fail
+ */
+
+fun generateRandomGraphicEvents(qtyEvents: Int, time: Int){
+    /**
+     * 1. Tap 2. Long tap 3. Scroll 4. Go back 5. Go home 6. Show recents 7. Turn
+     * on/off screen 8. Rotate landscape 9. Rotate portrait 10. Show keyboard 11.
+     * Show numeric keyboard 12. Enter input
+     */
+    val events = List(qtyEvents){Random.nextInt(12-1+1)+1}
+    //Starts to execute methods
+    val size = getScreenSize()
+    events.forEach{
+        var coordX = Random.nextInt(size[0] - 0 + 1)+0
+        var coordY = Random.nextInt(size[1] - 0 + 1) + 0;
+        when(it){
+            1 -> {
+                tap(coordX.toString(),coordY.toString())
+            }
+            2 -> {
+                val ms = Random.nextInt(100-1+1)+1
+                longTap(coordX.toString(), coordY.toString(), ms.toString())
+            }
+            3 -> {
+                val coordX1 = Random.nextInt(size[0] - 0 + 1)+0
+                val coordY1 = Random.nextInt(size[1] - 0 + 1)+0
+                val ms = Random.nextInt(1000- 1 + 1)+1
+                scroll(coordX.toString(), coordY.toString(),coordX1.toString(), coordY1.toString(),ms.toString())
+            }
+            4 -> goBack()
+            5 -> goHome()
+            6 -> showRecents()
+            7 -> turnOnScreen()
+            8 -> rotateLandscape()
+            9 -> rotatePortrait()
+            10 -> showKeyboard()
+            11 -> showNumKeyboard()
+            12 -> enterInput()
+        }
+        Thread.sleep(time.toLong())
+    }
+    println("End of events")
+}
+/**
+ * Shows the current focus in screen
+ *
+ * @return String with the actual focus and the package name
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             if there is no device
+ */
+fun getCurrentFocus(): String{
+    val commands = listOf("adb", "shell", "dumpsys", "window", "|", "grep", "-E",
+        "'mCurrentFocus'")
+    val answer = executeProcess2(commands, "GETTING CURRENT FOCUS", null, null)
+    return with(answer.first().split(" ")){
+        last().replace("}","")
+    }
+}
+/**
+ * Shows the focused app in screen
+ *
+ * @return String with the actual activity and the package name
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             if there is no device
+ */
+fun getFocusedApp(): String{
+    val commands = listOf("adb", "shell", "dumpsys", "window", "|", "grep", "-E", "'mFocusedApp'")
+    val answer = executeProcess2(commands, "GETTING FOCUSED APP", null, null)
+    return with(answer.first().split("\n").first() ){
+        val app = split(" ").let { list -> list.get(list.lastIndex-1) }
+        println(app)
+        app
+    }
+}
+/**
+ * Gets dimensions of device to know where you can tap
+ *
+ * @return an array with the x and y dimensions
+ * @throws Exception
+ *             if there is no device
+ */
+fun getScreenSize(): IntArray{
+    val commands = listOf("adb", "shell", "wm", "size")
+    val answer = executeProcess2(commands, "CHEKING DEVICE DIMENSIONS", null, null)
+    with(answer.first().split(" ").elementAt(2).replace("\\s+","")){
+        val ans = IntArray(2)
+        ans[0] = split("x").first().toInt()
+        ans[1] = split("x").elementAt(1).toInt()
+        return ans
+    }
+}
+
+/**
+ * Gets dimensions of device
+ *
+ * @return a string with the dimensions (ex: 1080x1920)
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             if there is no device
+ */
+fun getScreenDimensions():String{
+    val commands = listOf("adb", "shell", "wm", "size")
+    val answer = executeProcess2(commands, "CHEKING DEVICE DIMENSIONS", null, null)
+    return answer.first().split(":").first().replace("\\s+","")
+}
+/**
+ * Checks if it is at Home
+ *
+ * @return true if it is at home, false if it is not
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             if there is no emulator or device
+ */
+fun isHome(): Boolean{
+    var ans = false
+    val actual = getCurrentFocus()
+    println("Current focus: $actual")
+    if(actual.contains("launcher") || actual.contains("Launcher")){
+        ans = true
+    }
+    println("Is in home: $ans")
+    return ans
+}
+/**
+ * Starts an app if you dont know the name of the main activity
+ *
+ * @param packageName
+ *            is the package of your app
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             is something while executing the command happens
+ */
+fun startApp(packageName: String){
+    val commands = listOf("adb", "shell", "monkey", "-p", packageName, "-v", "1")
+    executeProcess2(commands, "STARTING APP", null, null)
+}
+/**
+ * Checks if any keyboard is open
+ *
+ * @return true is a keyboard is open, false in other case
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             is there is a problem executing command
+ */
+fun isKeyBoardOpen(): Boolean{
+    val commands = listOf("adb", "shell", "dumpsys", "input_method", "|", "grep", "mInputShown")
+    val answer = executeProcess2(commands, "CHEKING IF KEYBOARD IS SHOWN", null, null)
+    with(answer.first().split(" ").last()){
+        return split("=").first().replace("\\s+","").toBoolean()
+    }
+}
+
+/**
+ * Verifies which is the input type of a edit text
+ *
+ * @return 0 = no input available 1 = String 2 = Number without decimal or
+ *         negative 3 = Number with signs 4 = Number with decimal 5 = Phone
+ *         number 6 = Date and time
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ */
+fun checkInputType(): Int{
+    var ret: Int = 0
+    val commands = listOf("adb", "shell", "dumpsys", "input_method", "|", "grep", "-A1",
+        "'mCurrentTextBoxAttribute'")
+    val answer = executeProcess2(commands, "CHECKING IF KEYBOARD IS SHOWN", null, null)
+    with(answer.first().split("\n")){
+        val number = first().substring(first().indexOf("inputType="),first().indexOf("ime")).replace("\\s+","")
+        if(number == "0" || number == null || number.equals(null)){
+            ret = 0
+        }else if(number.endsWith("1")){
+            ret = 1
+        }else if(number.endsWith("2")){
+            ret = 2
+        }else if(number.contains("1002") || number.contains("2002")){
+            ret = 3
+        }else if(number.endsWith("3")){
+            ret = 4
+        }else if(number.endsWith("4")){
+            ret = 5
+        }else{
+        }
+    }
+    return ret
+}
+/**
+ * Gets the package of the apk that you want to test
+ *
+ * @param aapt
+ *            is the path where aapt is located
+ * @param apk
+ *            is the path where apk is located
+ * @return package name of the app
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             if any path is bad
+ */
+fun getPackageName(aapt: String, apk: String): String{
+    val commands = listOf(aapt, "dump", "badging", apk)
+    val answer = executeProcess2(commands, "GETTING PACKAGE NAME", null, null)
+    val ans = answer.first().substring(answer.first().indexOf("name="), answer.first().indexOf("versionCode"))
+    return ans.split("=").first().replace("'","").replace("\\s+","")
+}
+/**
+ * Gets the main activity of the apk given
+ *
+ * @param aapt
+ *            is the path where aapt is located
+ * @param apk
+ *            is the path where apk is located
+ * @return main activity of the app
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             if any path is bad
+ */
+fun getMainActivity(aapt: String, apk:String):String{
+    val commands = listOf(aapt, "dump", "badging", apk)
+    val answer = executeProcess2(commands, "GETTING MAIN ACTIVITY", null, null)
+
+    with(answer.first().split("\n")){
+        (find { it -> it.contains("launchable-activity") }?: "").let {
+            return if(it != ""){
+                val first = it.indexOf("'")
+                it.substring(first, it.indexOf("'",first+1))
+                    .let { it.replace("'", "") }
+                    .let { it.replace("\\s+","") }
+            }else{
+                throw RipException("There is no launchable activity")
+            }
+        }
+    }
+}
+
+/**
+ * Lists all available sensors on device
+ *
+ * @return a list with all the sensors
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             if there is no device connected
+ */
+fun getSensors(): String{
+    val commands = listOf("adb", "shell", "pm", "list", "features")
+    val answer = executeProcess2(commands, "GETTING AVAILABLE SENSORS", null, null)
+    return if(answer.first().isNotEmpty()){
+        answer.first().replace("feature:", "")
+    }else{""}
+}
+/**
+ * Search all the services related with the activity and service
+ *
+ * @param packageName
+ *            is the package of the apk
+ * @return all the services related with the app
+ * @throws RipException
+ * @throws IOException
+ * @throws Exception
+ *             if there is no device connected
+ */
+fun getServices(packageName: String): String{
+    val commands = listOf("adb", "shell", "dumpsys", "activity", "services", packageName)
+    val answer = executeProcess2(commands, "GETTING ACTIVITY SERVICES", null, null)
+    return if(answer.first().isNotEmpty()){
+        answer.first()
+    }else{""}
+}
+
+fun turnWifi(value: Boolean): String{
+    var sw = "OFF"
+    var wifiValue = "disable"
+    if(value){
+        wifiValue = "enable"
+        sw = "ON"
+    }
+    val commands = listOf("adb", "shell", "svc", "bluetooth", wifiValue)
+    val answer = executeProcess2(commands, "TURNING $sw -> WIFI", null, null)
+    return if(answer.first().isNotEmpty()){
+        answer.first()
+    }else{""}
+}
+
+fun turnBluetooth(value: Boolean): String{
+    var sw = "OFF"
+    var bluetoothValue = "disable"
+    if (value) {
+        bluetoothValue = "enable";
+        sw = "ON";
+    }
+
+    val commands = listOf("adb", "shell", "svc", "bluetooth", bluetoothValue)
+    val answer = executeProcess2(commands, "TURNING $sw -> BLUETOOTH", null, null)
+    return if(answer.first().isNotEmpty()){
+        answer.first()
+    }else{""}
+}
+
+fun turnMobileData(value: Boolean): String{
+    var sw = "OFF"
+    var dataValue = "disable"
+    if (value) {
+        dataValue = "enable"
+        sw = "ON"
+    }
+    val commands = listOf("adb", "shell", "svc", "data", dataValue)
+    val answer = executeProcess2(commands, "TURNING $sw -> DATA", null, null)
+    return if(answer.first().isNotEmpty()){
+        answer.first()
+    }else{""}
+}
+
+/**
+ * Rotates the device
+ *
+ * @param value
+ *            true landscape, false portrait
+ * @throws RipException
+ * @throws IOException
+ */
+fun rotateDevice(value: Boolean){
+    if(value){
+        rotateLandscape()
+    }else{
+        rotatePortrait()
+    }
+}
+
+
+fun turnInternet(value: Boolean): String{
+    return "${turnWifi(value)} \n ${turnNetworkLocation(value)}"
+}
+
+fun turnLocationServices(value: Boolean):String{
+    return "${turnGPS(value)} \n ${turnNetworkLocation(value)}"
+}
+
+fun turnGPS(value: Boolean): String{
+    var sw = "OFF"
+    var dataValue = "-gps"
+    if (value) {
+        dataValue = "+gps"
+        sw = "ON"
+    }
+    val commands = listOf("adb", "shell", "settings", "put", "secure", "location_providers_allowed",
+        dataValue)
+    val answer = executeProcess2(commands, "TURNING $sw -> GPS", null, null)
+    return if(answer.first().isNotEmpty()){
+        answer.first()
+    }else{""}
+}
+
+fun turnNetworkLocation(value: Boolean): String{
+    var sw = "OFF"
+    var dataValue = "-network"
+    if (value) {
+        dataValue = "+network"
+        sw = "ON"
+    }
+    val commands = listOf("adb", "shell", "settings", "put", "secure", "location_providers_allowed",
+        dataValue)
+    val answer = executeProcess2(commands, "TURNING" + sw + "-> NETWORK LOCATION SERVICES", null, null)
+    return if(answer.first().isNotEmpty()){
+        answer.first()
+    }else{""}
+}
+
+fun readWebViewConsole(){
+    simpleTryCatch {
+        val proc = Runtime.getRuntime().exec("adb logcat chromium:D SystemWebViewClient:D *:S")
+        val inputStreamReader = InputStreamReader(proc.inputStream)
+        BufferedReader(inputStreamReader).use {
+            it.forEachLine {
+                println(it)
+            }
+        }
+        proc.waitFor()
     }
 }
